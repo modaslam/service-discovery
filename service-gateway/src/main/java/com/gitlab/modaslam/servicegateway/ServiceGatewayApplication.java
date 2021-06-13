108bd197a66870d8a211052b1c3019501f56db3c
Mohammed Aslam P. A <aslam.puthukode@gmail.com>
2021-06-13 21:32:58 +0530

    Implemented service discovery

--- service-gateway/src/main/java/com/gitlab/modaslam/servicegateway/ServiceGatewayApplication.java
+++ service-gateway/src/main/java/com/gitlab/modaslam/servicegateway/ServiceGatewayApplication.java
@@ -2,7 +2,9 @@ package com.gitlab.modaslam.servicegateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceGatewayApplication {
 
